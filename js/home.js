ddc2c80f578bf2abdf1c002d819d2cd0b92aa30f
Faraz Patankar <farazpatankar@gmail.com>
2016-03-12 23:11:20 -0500

    Set up materialize, multi-screen and typed

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".typed").typed({
+    strings: ["Ruby on Rails Developer", "Manchester United Fan", "Aspiring Entrepreneur", "Tech Enthusiast", "Avid Learner"],
+    typeSpeed: 0,
+    loop: true
+  });
+});
+
+$(document).ready(function() {
+  MultiScreen.init();
+});
